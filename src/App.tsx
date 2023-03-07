bbd1e1a3f257400d21f6dc0692c1ed14148c3b0b
Elbert Jose <e.jose@reply.com>
2023-03-07 19:45:33 -0300

    Create global style

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/theme/default"
+
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+        <div>
+          <h1>Ola</h1>
+        </div>
+    </ThemeProvider>
+    
+  )
+}    
